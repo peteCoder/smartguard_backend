253cbf27d0d60890784aa4e7247afbd051a85728
ghostpete <ghostcoderme@gmail.com>
2025-06-08 22:17:06 +0100

    added pydantic-settings to requirements.txt

--- config.py
+++ config.py
@@ -5,12 +5,9 @@ from typing import List
 from pydantic import Field, field_validator
 from typing import Union
 
-# Optional: If you still want to load .env manually
-# load_dotenv()
 
 class Settings(BaseSettings):
     API_SECRET_KEY: str = Field("defaultsecret", env="API_SECRET_KEY")
-    # ALLOWED_ORIGINS: List[str] = Field(["*"], env="ALLOWED_ORIGINS")
     ML_MODEL_PATH: str = Field("models/phishing_model.pkl", env="ML_MODEL_PATH")
     REPORT_OUTPUT_DIR: str = Field("static/reports", env="REPORT_OUTPUT_DIR")
     DEBUG: bool = Field(True, env="DEBUG")
